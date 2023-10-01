45d2cb8c591e0abf3c63e17e55f7b984a70f9a78
Hong Thy Nguyen <hongthy3101@gmail.com>
2023-10-01 14:51:15 -0400

    Create Navbar - stop at 35:19 on video

--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
@@ -11,17 +12,35 @@ const HeroSection = () => {
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ">
               Hello, I'm{" "}
             </span>
-            Amy
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Amy",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "Software Engineer",
+                1000,
+                "Cybersecurity Engineer",
+                1000,
+                "Web Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
           <p className="text-white text-base sm:text-lg mb-6 lg:text-xl ">
-            lorem ipsum dolor sit am
+            lorem ipsum dolor sit am lorem ipsum dolor sit am lorem ipsum dolor
           </p>
           <div>
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-white hover:bg-slate-200 text-white">
               Hire me
             </button>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white border border-white mt-3">
-              Download Resume
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-transparent bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-black hover:bg-slate-400 rounded-full px-5 py-2">
+                Download Resume
+              </span>
             </button>
           </div>
         </div>
